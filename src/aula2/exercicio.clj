901125f4a5399358146bf10e9c472fa9fe6c6c44
Joice Fogaca <joice.fogaca@joice.fogaca>
2022-09-30 17:16:42 -0300

    resolvendo aula 2

--- src/aula2/exercicio.clj
+++ src/aula2/exercicio.clj
@@ -4,12 +4,21 @@
 
 ;; criar um vetor com nome de clientes que conter� todos os clientes
 ;; de ambas as colec�es do namespace `aula2.data`.
-()
+(into data/clientes-lista data/clientes-vetor) ;;retorna lista
+(into [] (into data/clientes-lista data/clientes-vetor))
 
 
 ;; adicionar um cliente no vetor `clientes`.
+(conj data/clientes-vetor {:first-name         "Renata"
+                            :last-name         "Silva"
+                            :age                39
+                            :last-year-revenue 7000
+                            :location           "SP"})
 
 ;; retornar o cliente na posic�o `p` do vetor clientes.
+(def pega-por-posicao-vet
+  [posicao])
+(get data/clientes-vetor posicao))
 
 
 ;; retornar o nome do cliente mais novo do vetor `clientes`.
